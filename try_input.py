f005a64ea9930610c5b1c79ff1ae638aad890c80
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-13 14:57:08 +0000

    another method of printing out a number

--- try_input.py
+++ try_input.py
@@ -2,3 +2,9 @@
 user_input = input("Enter a number: ")
 
 print(int(user_input) ** 2)
+
+##############
+
+# OR (make sure you use user_input WITHIN **2 and not as above)
+user_input = int(input("Enter a number: "))
+print(user_input ** 2)
