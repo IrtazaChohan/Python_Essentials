cd74de78fa8c8f878e89cf211f6b49312156c963
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-16 11:48:17 +0000

    added in print method

--- simple_datetime.py
+++ simple_datetime.py
@@ -1,4 +1,5 @@
 import datetime
 datetime.datetime.now()
 
+print(datetime.datetime.now())
 # exit() - this will exit the python terminal in VSC when you run this code you don't want to be in the terminal.
