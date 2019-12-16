81f3fb7811141df9e619f16b6a7cf3d41d0fccb0
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-16 11:55:12 +0000

    Another method of showing datetime

--- simple_datetime.py
+++ simple_datetime.py
@@ -1,7 +1,7 @@
 import datetime
 datetime.datetime.now()
 
-# add everything on the same line
+# add everything on the same line - separate with comma
 print("The date and time today is: ", datetime.datetime.now())
 
 #or print datetime on a seprate line
@@ -9,3 +9,8 @@ print("The date and time today is: ", datetime.datetime.now())
 
 
 #NOTE: exit() - this will exit the python terminal in VSC when you run this code you don't want to be in the terminal.
+
+# Another method of showing datetime
+import datetime
+x = datetime.datetime.now()
+print(x)
