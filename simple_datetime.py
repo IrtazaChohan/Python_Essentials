7884e26fc2bec5748cec4f8954f21d702db1dcbb
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-16 11:51:04 +0000

    added info to the script

--- simple_datetime.py
+++ simple_datetime.py
@@ -1,5 +1,5 @@
 import datetime
 datetime.datetime.now()
-
+print("The date and time today is: ")
 print(datetime.datetime.now())
 # exit() - this will exit the python terminal in VSC when you run this code you don't want to be in the terminal.
