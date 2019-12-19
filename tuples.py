f4ccc9d05918ee7a6d3ce513a14ab80d2026b8da
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-19 19:36:24 +0000

    info added for tuples differences

---
+++ tuples.py
@@ -0,0 +1,10 @@
+
+# tuple is just like a list but without square brackets!
+# main difference is that tuples are immutables - lists are not.
+
+monday_temperatures = {1, 4, 5}
+
+list_temperatures = [1, 4, 5]
+
+print(monday_temperatures)
+
