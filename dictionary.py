0664117eebb958c8d3bb359d5e42179238fd896a
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-19 19:28:19 +0000

    added in sum calculation of dictionary

--- dictionary.py
+++ dictionary.py
@@ -2,3 +2,14 @@
 # Dictionaries are key value pairs
 student_grades = {"Mary": 9.1,"Sim": 8.8,"John": 7.5}
 
+student_grades.keys()
+
+student_grades.values()
+
+len(student_grades)
+
+print((sum(student_grades.values())) / (len(student_grades)))
+
+dir(dict)
+
+dir(dict.values)
