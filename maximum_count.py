3a1b95dd8b805ad43d736b72bc26541a6d192772
Irtaza Chohan <irtazachohan@gmail.com>
2019-12-19 19:13:18 +0000

    added count check

--- maximum_count.py
+++ maximum_count.py
@@ -1,4 +1,15 @@
 student_grades = [9.1, 8.8, 7.5]
-max_value = 
+max_value = max(student_grades)
 print(max_value)
+
+# find builtin functions...ie max as above to use
+dir(__builtins__)
+
+
+#####################
+
+# counts how many 10's are in a list
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+
+student_grades.count(10.0)
 
